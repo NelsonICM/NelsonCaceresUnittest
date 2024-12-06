c8f55a30398383389daa66bd4e003c5d3749110b
NelsonICM <107224948+NelsonICM@users.noreply.github.com>
2024-12-05 20:45:00 -0600

    Update UserServiceTest.java

--- src/test/java/com/mayab/quality/logginunittest/service/UserServiceTest.java
+++ src/test/java/com/mayab/quality/logginunittest/service/UserServiceTest.java
@@ -53,7 +53,7 @@ class UserServiceTest {
         service.createUser("name", "email", "password");
 
         int sizeAfter = db.size();
-        assertThat(sizeAfter, is(sizeBefore + ));
+        assertThat(sizeAfter, is(sizeBefore + 1));
     }
 
     // Second test: Handle duplicate email
